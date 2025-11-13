ca447d3f4939e6b4e6e713b9df966a8d3f9ee489
ImBdang <imbdang@gmail.com>
2025-11-13 16:09:08 +0700

    update

--- inc/flash_stm32f103c8t6.h
+++ inc/flash_stm32f103c8t6.h
@@ -5,10 +5,19 @@
 #include <stdint.h>
 
 /*===============FLASH REGISTER==============*/
-typedef struct{
-    volatile uint32_t ACR;
+typedef struct {
+    volatile uint32_t ACR;       
+    volatile uint32_t KEYR;      
+    volatile uint32_t OPTKEYR;   
+    volatile uint32_t SR;       
+    volatile uint32_t CR;   
+    volatile uint32_t AR;       
+    uint32_t RESERVED;           
+    volatile uint32_t OBR;       
+    volatile uint32_t WRPR;      
 } FLASH_TypeDef;
 
 #define FLASH ((volatile FLASH_TypeDef*)0x40022000)
+
 
 #endif
